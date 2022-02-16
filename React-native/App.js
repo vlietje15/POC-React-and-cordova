d4e690742de0b6a2eae3ead74255a0f1fa3b486b
Vliet,Lars L.E.M. van der <l.vandervliet@student.fontys.nl>
2022-02-16 10:13:16 +0100

    Start home page

--- React-native/App.js
+++ React-native/App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Fuck yeah</Text>
+      
+      <View style={styles.carContainer}>
+        <ImageBackground
+          source={require('./TeslaAssets/assets/images/ModelX.jpeg')}
+          style={styles.image}
+        />
+        
+        <View style={styles.titles}>
+          <Text style={styles.title}>Model S</Text>
+          <Text style={styles.subtitle}>Starting at $69,420</Text>
+        </View>  
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +26,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fbf',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  carContainer: {
+    width: '100%',
+    height: '100%',
+  },
+
+  titles: {
+    marginTop: '30%',
+    width: '100%',
+    alignItems: 'center',
+  },
+
+  title: {
+    fontSize: 40,
+    fontWeight: '500',
+  },
+
+  subtitle: {
+    fontSize: 16,
+    color: '#5c5e62'
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    position: 'absolute',
+  }
 });
